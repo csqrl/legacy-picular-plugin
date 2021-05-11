cef28a1f0c26c876549ffde7e7d834bb96e6491b
Cam <21029087+csqrl@users.noreply.github.com>
2021-05-11 13:41:15 +0100

    Update ButtonBase.lua

--- src/Components/Util/ButtonBase.lua
+++ src/Components/Util/ButtonBase.lua
@@ -11,50 +11,76 @@ function ButtonBase.init(self: RoactComponent)
         press = false,
     })
 
-    self._onClick = function(rbx: Instance, ...)
-        if self.props.disabled then
-            return
-        end
-
-        if type(self.onClick) == "function" then
-            local result = self.onClick(rbx, ...)
-
-            if result == false then
+    self._onClick = function(originalCallback)
+        return function(rbx: Instance, ...)
+            if self.props.disabled then
                 return
             end
-        end
 
-        if type(self.props.onClick) == "function" then
-            self.props.onClick(rbx, ...)
+            if type(self.onClick) == "function" then
+                local result = self.onClick(rbx, ...)
+
+                if result == false then
+                    return
+                end
+            end
+
+            if type(originalCallback) == "function" then
+                originalCallback(rbx, ...)
+            end
+
+            if type(self.props.onClick) == "function" then
+                self.props.onClick(rbx, ...)
+            end
         end
     end
 
-    self.onInputBegan = function(_, input: InputObject)
-        if self.props.disabled then
-            return
-        end
+    self._onInputBegan = function(originalCallback)
+        return function(rbx: Instance, input: InputObject, ...)
+            if self.props.disabled then
+                return
+            end
 
-        if input.UserInputType.Name:match("MouseButton%d+") or input.UserInputType == Enum.UserInputType.Touch then
-            self:setState({ press = true })
-        elseif input.UserInputType == Enum.UserInputType.MouseMovement then
-            self:setState({ hover = true })
+            if type(self.onInputBegan) == "function" then
+                self.onInputBegan(rbx, input, ...)
+            end
+
+            if type(originalCallback) == "function" then
+                originalCallback(rbx, input, ...)
+            end
+
+            if input.UserInputType.Name:match("MouseButton%d+") or input.UserInputType == Enum.UserInputType.Touch then
+                self:setState({ press = true })
+            elseif input.UserInputType == Enum.UserInputType.MouseMovement then
+                self:setState({ hover = true })
+            end
         end
     end
 
-    self.onInputEnded = function(_, input: InputObject)
-        if input.UserInputType.Name:match("MouseButton%d+") or input.UserInputType == Enum.UserInputType.Touch then
-            self:setState({ press = false })
-        elseif input.UserInputType == Enum.UserInputType.MouseMovement then
-            self:setState({ hover = false, press = false })
+    self._onInputEnded = function(originalCallback)
+        return function(rbx: Instance, input: InputObject, ...)
+            if type(self.onInputEnded) == "function" then
+                self.onInputEnded(rbx, input, ...)
+            end
+
+            if type(originalCallback) == "function" then
+                originalCallback(rbx, input, ...)
+            end
+
+            if input.UserInputType.Name:match("MouseButton%d+") or input.UserInputType == Enum.UserInputType.Touch then
+                self:setState({ press = false })
+            elseif input.UserInputType == Enum.UserInputType.MouseMovement then
+                self:setState({ hover = false, press = false })
+            end
         end
     end
 end
 
 function ButtonBase.render(self: RoactComponent, element: RoactElement)
     return Roact.createElement(element.component, Llama.Dictionary.merge(element.props, {
-        [Roact.Event.Activated] = self._onClick,
-        [Roact.Event.InputBegan] = self.onInputBegan,
-        [Roact.Event.InputEnded] = self.onInputEnded,
+        [Roact.Event.Activated] = self._onClick(element.props[Roact.Event.Activated]),
+        [Roact.Event.InputBegan] = self._onInputBegan(element.props[Roact.Event.InputBegan]),
+        [Roact.Event.InputEnded] = self._onInputEnded(element.props[Roact.Event.InputEnded]),
     }))
 end
 
